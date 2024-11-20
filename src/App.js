7f3709afc0f5724c27427b435c7f1fa246f87ef7
Турик Александр <alex@PLATANN.platan.ua>
2024-11-20 14:54:32 +0300

    start

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     dfsf
+      <input type="text" />
+      <div className="wrapper">
+        <p>Остатки: 50</p>
+      </div>
     </div>
   );
 }
